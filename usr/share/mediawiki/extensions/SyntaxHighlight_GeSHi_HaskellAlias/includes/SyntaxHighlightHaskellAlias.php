4f093b3be81a7031dd27bd8160753aeb833663d2
Tomas Račys <taezaz@gmail.com>
2023-01-30 23:43:57 +0200

    Move <haskell/> and <hask/> aliases for syntax highlighting into a new extension - remove related logic from LocalSettings.php

---
+++ usr/share/mediawiki/extensions/SyntaxHighlight_GeSHi_HaskellAlias/includes/SyntaxHighlightHaskellAlias.php
@@ -0,0 +1,24 @@
+<?php
+
+class SyntaxHighlightHaskellAliasHooks
+{
+    public static function onParserFirstCallInit(Parser &$parser)
+    {
+        $parser->setHook('haskell', [self::class, 'haskellKeywordHook']);
+        $parser->setHook('hask', [self::class, 'haskKeywordHook']);
+    }
+
+    public static function haskellKeywordHook($text, $args = array(), $parser)
+    {
+        $args['lang'] = 'haskell';
+        return SyntaxHighlight_GeSHi::parserHook($text, $args, $parser);
+    }
+
+    public static function haskKeywordHook($text, $args = array(), $parser)
+    {
+        $args['lang'] = 'haskell';
+        $args['enclose'] = 'none';
+        $out = SyntaxHighlight_GeSHi::parserHook($text, $args, $parser);
+        return '<span class="inline-code">' . $out . '</span>';
+    }
+}
