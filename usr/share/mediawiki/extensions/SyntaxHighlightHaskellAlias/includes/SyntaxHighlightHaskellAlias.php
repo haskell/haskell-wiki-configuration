4cf6f408d80a2140ed998c3991df462092b384b7
Howard B. Golden <howard.b.golden@gmail.com>
2023-04-25 09:37:45 -0700

    Update names

--- usr/share/mediawiki/extensions/SyntaxHighlightHaskellAlias/includes/SyntaxHighlightHaskellAlias.php
+++ usr/share/mediawiki/extensions/SyntaxHighlightHaskellAlias/includes/SyntaxHighlightHaskellAlias.php
@@ -1,25 +1,26 @@
 <?php
 
+use Mediawiki\SyntaxHighlight;
+
 class SyntaxHighlightHaskellAliasHooks
 {
-    public static function onParserFirstCallInit(Parser &$parser)
-    {
-        $parser->setHook('haskell', [self::class, 'haskellKeywordHook']);
-        $parser->setHook('hask', [self::class, 'haskKeywordHook']);
-    }
+  public static function onParserFirstCallInit(Parser &$parser)
+  {
+    $parser->setHook('haskell', [self::class, 'haskellKeywordHook']);
+    $parser->setHook('hask', [self::class, 'haskKeywordHook']);
+  }
 
-    public static function haskellKeywordHook($text, $args = array(), $parser)
-    {
-        $args['lang'] = 'haskell';
-        return SyntaxHighlight_GeSHi::parserHook($text, $args, $parser);
-    }
+  public static function haskellKeywordHook($text, $args = array(), $parser)
+  {
+    $args['lang'] = 'haskell';
+    return SyntaxHighlight::parserHook($text, $args, $parser);
+  }
 
-    public static function haskKeywordHook($text, $args = array(), $parser)
-    {
-        $args['lang'] = 'haskell';
-        $args['enclose'] = 'none';
-        $out = SyntaxHighlight_GeSHi::parserHook($text, $args, $parser);
-        return '<span class="inline-code">' . $out . '</span>';
-    }
+  public static function haskKeywordHook($text, $args = array(), $parser)
+  {
+    $args['lang'] = 'haskell';
+    $args['enclose'] = 'none';
+    $out = SyntaxHighlight::parserHook($text, $args, $parser);
+    return '<span class="inline-code">' . $out . '</span>';
+  }
 }
-
