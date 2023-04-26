2402f6a6a67562afe40835825bab4ef17e4d45df
Howard B. Golden <howard.b.golden@gmail.com>
2023-04-25 21:06:06 -0700

    Additional corrections

--- usr/share/mediawiki/extensions/SyntaxHighlightHaskellAlias/includes/SyntaxHighlightHaskellAlias.php
+++ usr/share/mediawiki/extensions/SyntaxHighlightHaskellAlias/includes/SyntaxHighlightHaskellAlias.php
@@ -2,7 +2,7 @@
 
 class SyntaxHighlightHaskellAlias
 {
-  public static function onParserFirstCallInit(Parser &$parser)
+  public static function onParserFirstCallInit(Parser $parser)
   {
     $parser->setHook( 'haskell', [ self::class, 'renderHaskellTag' ] );
     $parser->setHook( 'hask', [ self::class, 'renderHaskTag' ] );
@@ -10,13 +10,13 @@ class SyntaxHighlightHaskellAlias
 
   public static function renderHaskellTag( $input, array $args, Parser $parser, PPFrame $frame )
   {
-    $output = $parser->recursiveTagParse( $input, $frame );
-    return '<syntaxhighlight lang="haskell">' . $output . '</syntaxhighlight>';
+    $output = '<hask />' . $parser->recursiveTagParse( '<syntaxhighlight lang="haskell">' . $input . '</syntaxhighlight>', $frame );
+    return $output;
   }
 
   public static function renderHaskTag( $input, array $args, Parser $parser, PPFrame $frame )
   {
-    $output = $parser->recursiveTagParse( $input, $frame );
-    return '<syntaxhighlight lang="haskell" inline>' . $output . '</syntaxhighlight>';
+    $output = '<hask />' . $parser->recursiveTagParse( '<syntaxhighlight lang="haskell" inline>' . $input . '</syntaxhighlight>', $frame );
+    return $output;
   }
 }
