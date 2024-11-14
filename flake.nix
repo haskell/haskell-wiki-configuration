88af4c26ff9bd548e35ea03406d8284df7d81f40
davean <davean@xkcd.com>
2024-11-13 20:48:38 -0500

    Updates to flake and move extension.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   outputs = { self, nixpkgs }: {
     nixosModules.hawiki = { config, pkgs, lib, ... }:
-      let let cfg = config.services.hawiki;
+      with lib;
+      let cfg = config.services.hawiki;
       in {
         options = {
           services.hawiki = {
@@ -27,8 +28,6 @@
           ];
           
           containers.hawiki = let passPath = config.sops.secrets.hawiki-pass.path; in {
-            system.stateVersion = "24.05";
-            boot.isContainer = true;   
             autoStart = true;
             
             bindMounts = {
@@ -41,63 +40,67 @@
                 isReadOnly = false;
               };
             };
+
+            config = { config, pkgs, lib, ... }: {
+              system.stateVersion = "24.05";
             
-            networking.useDHCP = false;
-            networking = {
-              firewall = {
+              networking.useDHCP = false;
+              networking = {
+                firewall = {
+                  enable = true;
+                  allowedTCPPorts = [ 80 ];
+                };
+                useHostResolvConf = lib.mkForce false;
+              };
+              
+              services.mediawiki = {
                 enable = true;
-                allowedTCPPorts = [ 80 ];
+                webserver = "nginx";
+                url = cfg.url;
+                name = "HaskellWiki";
+                passwordSender = "haskell-cafe@haskell.org";
+                passwordFile = cfg.passFile;
+                
+                httpd.virtualHost = {
+                  hostName = "wiki.haskell.org";
+                };
+                
+                extensions = {
+                  Cite = null;
+                  SyntaxHighlight_GeSHi = null;
+                  Math = null;
+                  #Wikidiff2 = null;                                                                                                                                                                  
+                  Interwiki = null;
+                  WikiEditor = null;
+                  CiteThisPage = null;
+                  ConfirmEdit = null;
+                  Gadgets = null;
+                  ImageMap = null;
+                  InputBox = null;
+                  # LocalisationUpdate = null; # Depricated                                                                                                                                           
+                  Nuke = null;
+                  ParserFunctions = null;
+                  Poem = null;
+                  # Renameuser = null; # Part of mediawiki proper since 1.40                                                                                                                          
+                  SpamBlacklist = null;
+                  TitleBlacklist = null;
+                  CollapsibleVector = pkgs.fetchgit
+                    { url = "https://gerrit.wikimedia.org/r/mediawiki/extensions/CollapsibleVector";
+                      rev = "3fddfb23f86061bbfafda6554b1d7c5f11edfcac";
+                      sha256 = "0fl80l3xi4fl98msmbwdi8vzynaaa9r6lp37hpb7faxhpkzb9wxh";
+                    };
+                  SyntaxHighlightHaskellAlias = ./SyntaxHighlightHaskellAlias;
+                };
+                
+                database = {
+                  type = "mysql";
+                  createLocally = true;
+                };
               };
-              useHostResolvConf = lib.mkForce false;
-            };
-            
-            services.mediawiki = {
-              enable = true;
-              webserver = "nginx";
-              url = cfg.url;
-              name = "HaskellWiki";
-              passwordSender = "haskell-cafe@haskell.org";
-              passwordFile = cfg.passFile;
               
-              httpd.virtualHost = {
-                hostName = "wiki.haskell.org";
+              services.memcached = {
+                enable = true;
               };
-              
-              extensions = {
-                Cite = null;
-                SyntaxHighlight_GeSHi = null;
-                Math = null;
-                #Wikidiff2 = null;                                                                                                                                                                  
-                Interwiki = null;
-                WikiEditor = null;
-                CiteThisPage = null;
-                ConfirmEdit = null;
-                Gadgets = null;
-                ImageMap = null;
-                InputBox = null;
-                # LocalisationUpdate = null; # Depricated                                                                                                                                           
-                Nuke = null;
-                ParserFunctions = null;
-                Poem = null;
-                # Renameuser = null; # Part of mediawiki proper since 1.40                                                                                                                          
-                SpamBlacklist = null;
-                TitleBlacklist = null;
-                CollapsibleVector = pkgs.fetchgit
-                  { url = "https://gerrit.wikimedia.org/r/mediawiki/extensions/CollapsibleVector";
-                    rev = "3fddfb23f86061bbfafda6554b1d7c5f11edfcac";
-                    sha256 = "0fl80l3xi4fl98msmbwdi8vzynaaa9r6lp37hpb7faxhpkzb9wxh";
-                  };
-                SyntaxHighlightHaskellAlias = ./SyntaxHighlightHaskellAlias;
-              };
-              
-              database = {
-                type = "mysql";
-                createLocally = true;
-              };
-            };
-            
-            services.memcached = {
-              enable = true;
             };
           };
         };
